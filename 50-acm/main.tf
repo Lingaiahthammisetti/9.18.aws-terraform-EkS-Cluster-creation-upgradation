ed6b8d32a5825f6ef9b00d2deb30d35868ce40b3
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-16 18:17:57 +0530

    terraform msg

--- 50-acm/main.tf
+++ 50-acm/main.tf
@@ -10,12 +10,12 @@ resource "aws_acm_certificate" "expense" {
         }
     )
 }
-resource "aws_rout53" "expense" {
+resource "aws_route53_record" "expense" {
     for_each = {
-        for dvo in aws_acm_certificate.domain_validation_options : dvo.domain_name => {
+        for dvo in aws_acm_certificate.expense.domain_validation_options : dvo.domain_name => {
             name = dvo.resource_record_name
             record = dvo.resource_record_value
-            type = dvo.resource_record_types
+            type = dvo.resource_record_type
         }
     }
     allow_overwrite = true
@@ -27,7 +27,7 @@ resource "aws_rout53" "expense" {
 }
 resource "aws_acm_certificate_validation" "expense" {
     certificate_arn = aws_acm_certificate.expense.arn
-    validation_record_fqdns = [for record in aws_rout53_record.expense : record.fqdn]
+    validation_record_fqdns = [for record in aws_route53_record.expense : record.fqdn]
 }
 
 
